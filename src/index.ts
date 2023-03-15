332f6dc39cf1720122cf1b05d5a8c13f131c5212
p1ass <me@p1ass.com>
2023-03-15 21:45:11 +0900

    LLMChainを試す

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import { OpenAI } from 'langchain'
+import { PromptTemplate } from 'langchain/prompts'
+import { LLMChain } from 'langchain/chains'
 
 const OPEN_AI_KEY = process.env.OPENAI_API_KEY
 
-const model = new OpenAI({ openAIApiKey: OPEN_AI_KEY, temperature: 0.9 })
-
 async function main() {
-  const res = await model.call('hello')
+  const model = new OpenAI({ openAIApiKey: OPEN_AI_KEY, temperature: 0.9 })
+
+  // Setup template
+  const template = 'What is a good name for a company that makes {product}?'
+  const prompt = new PromptTemplate({
+    template: template,
+    inputVariables: ['product'],
+  })
+
+  // Build LLM chain
+  const chain = new LLMChain({ llm: model, prompt: prompt })
+
+  const res = await chain.call({ product: 'colorful sock' })
   console.log(res)
 }
 
