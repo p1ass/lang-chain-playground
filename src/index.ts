011528ac4e10ddd8642579e3117e8675687b1c93
p1ass <me@p1ass.com>
2023-03-15 21:39:49 +0900

    add tsconfig

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const OPEN_AI_KEY = process.env.OPENAI_API_KEY
 const model = new OpenAI({ openAIApiKey: OPEN_AI_KEY, temperature: 0.9 })
 
 async function main() {
-  const res = await model.call('ts-nodeでESMを読み込む方法は？')
+  const res = await model.call('hello')
   console.log(res)
 }
 
